abfa48f382a4909ad61de3b39c3571c971af1684
kohry <gorakgarak@gorakgarak.com>
2017-10-14 14:00:51 +0900

    contour

--- app/src/main/java/com/gorakgarak/anpr/MainActivity.kt
+++ app/src/main/java/com/gorakgarak/anpr/MainActivity.kt
@@ -1,3 +1,4 @@
+
 package com.gorakgarak.anpr
 
 import android.Manifest
@@ -13,10 +14,15 @@ import org.opencv.android.BaseLoaderCallback
 import org.opencv.android.CameraBridgeViewBase
 import org.opencv.android.LoaderCallbackInterface
 import org.opencv.android.OpenCVLoader
-import org.opencv.core.CvType
-import org.opencv.core.Mat
-import org.opencv.core.Size
+import org.opencv.core.*
+import org.opencv.core.CvType.CV_8UC1
 import org.opencv.imgproc.Imgproc
+import org.opencv.imgproc.Imgproc.*
+import java.util.*
+import org.opencv.core.CvType
+import org.opencv.core.MatOfPoint2f
+import org.opencv.core.MatOfPoint
+
 
 
 
@@ -122,11 +128,95 @@ class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListe
 
         Log.d(TAG, "1-4) Threshold")
         val element: Mat = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, Size(17.0, 3.0))
-        Imgproc.morphologyEx(matThreshold, matThreshold, CV_MOP_CLOSE, element)
+        Imgproc.morphologyEx(matThreshold, matThreshold, Imgproc.MORPH_CLOSE, element)
+
+        Log.d(TAG, "1-5) Find contour of possible plates")
+        val contourList: List<MatOfPoint> = emptyList()
+        Imgproc.findContours(matThreshold, contourList, null, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE)
+
+        Log.d(TAG, "1-6) Get rectangle from the contours")
+        val rectList: MutableList<RotatedRect> = mutableListOf()
+
+        //Convert MapOfPoint to MatOfPoint2f
+        contourList.forEach {
+            val p = MatOfPoint2f()
+            it.convertTo(p, CvType.CV_32F)
+            rectList.add(Imgproc.minAreaRect(p))
+        }
+
+        val input = inputFrame.rgba()
+        val result = Mat()
+        input.copyTo(result)
+        drawContours(result, contourList, -1, Scalar(200.0, 0.0, 0.0), 1)
+
+        Log.d(TAG, "1-6) Floodfill algorithm from more clear contour box")
+        floodFill(input, result, rectList)
+
+        return result
+    }
+
+    fun floodFill(input: Mat, result:Mat, rects: MutableList<RotatedRect>) {
+
+        rects.forEach {
+            //For better rect cropping for each possible box
+            //Make floodfill algorithm because the plate has white background
+            //And then we can retrieve more clearly the contour box
+            circle(result, it.center, 3, Scalar(0.0,255.0,0.0), -1);
+
+            val minSize = if (it.size.width < it.size.height) it.size.width * 0.5 else it.size.height * 0.5
+
+            var mask: Mat = Mat()
+            mask.create(input.rows() + 2, input.cols() + 2, CvType.CV_8UC1)
+            mask = Mat.zeros(mask.size(), CvType.CV_8UC3)
+
+            val loDiff = 30.0
+            val upDiff = 30.0
+            val connectivity = 4
+            val newMaskVal = 255
+            val seedNum = 10
+            val ccomp : Rect = Rect()
+            val flags = connectivity + (newMaskVal.shl(8) + FLOODFILL_FIXED_RANGE + FLOODFILL_MASK_ONLY)
+
+            (0 .. seedNum).forEach {
+                val num = Random().nextInt()
+                val seed:Point = Point()
+                seed.x=rects.get(it).center.x + num % (minSize-(minSize/2))
+                seed.y=rects.get(it).center.y + num % (minSize-(minSize/2))
+                circle(result, seed, 1, Scalar(0.0,255.0,255.0), -1);
+                val area = floodFill(input, mask, seed, Scalar(255.0, 0.0, 0.0), ccomp , Scalar(loDiff, loDiff, loDiff), Scalar(upDiff, upDiff, upDiff), flags)
+            }
+
+//            val pointsOfInterest: List<Point> = mutableListOf()
+
+//            vector<Point> pointsInterest;
+//            Mat_<uchar>::iterator itMask= mask.begin<uchar>();
+//            Mat_<uchar>::iterator end= mask.end<uchar>();
+//            for( ; itMask!=end; ++itMask)
+//            if(*itMask==255)
+//            pointsInterest.push_back(itMask.pos());
+
+        }
 
 
 
-        return matThreshold
+    }
+
+
+    fun verifySizes(candidate: RotatedRect): Boolean {
+
+        val error = 0.4
+        val aspect = 4.7272
+        val min = 15 * aspect * 15
+        val max = 125 * aspect * 125
+        val rmin = aspect - aspect * error
+        val rmax = aspect + aspect * error
+        val area = candidate.size.height * candidate.size.width;
+
+        var r = candidate.size.width / candidate.size.height
+        if (r < 1) r = 1 / r
+
+        return !(( area < min || area > max ) || ( r < rmin || r > rmax ))
+
     }
 
     external fun salt(matAddrGray: Long, nbrElem: Int)
